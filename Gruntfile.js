add020276f5b98d0e0ecaeeb2a428e2a38b55c29
Josh Silverman <joshs.silverman@gmail.com>
2014-08-09 14:17:51 -0400

    attempt with build pack and no post install

--- Gruntfile.js
+++ Gruntfile.js
@@ -437,6 +437,8 @@ module.exports = function (grunt) {
     'htmlmin'
   ]);
 
+  grunt.registerTask('heroku:production', 'build');
+
   grunt.registerTask('default', [
     'newer:jshint',
     'test',
