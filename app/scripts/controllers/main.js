5c9aca3ed87fecb36b1a258bba966dc325146ce9
Josh Silverman <joshs.silverman@gmail.com>
2014-08-11 09:17:50 -0400

    working path for example

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('ngApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('http://localhost:8080/users.json').success(function(data) {
+    $http.get('/users.json').success(function(data) {
       debugger;
       console.log(data);
     });
