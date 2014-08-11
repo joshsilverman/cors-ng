b44caf46fa964f1b6cc4cd1c785d96b2c4da9fb1
Josh Silverman <joshs.silverman@gmail.com>
2014-08-10 21:49:05 -0400

    attempts at cors demo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,9 @@
  * Controller of the ngApp
  */
 angular.module('ngApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('http://localhost:8080/users.json').success(function(data) {
+      debugger;
+      console.log(data);
+    });
   });
